c99bffcdc209dd7bbe5593df7e6cf315c407e554
Ashwini Sahu <46531635+ASHWIN990@users.noreply.github.com>
2019-02-19 16:32:25 +0530

    Create auto-install.sh

---
+++ auto-install.sh
@@ -0,0 +1,23 @@
+echo -e "\e[1;91mINSTALLING SUDO IN TERMUX\e[0m"
+
+apt-get update
+
+apt-get upgrade -y
+
+apt-get install git
+
+apt-get install ncurses-utils -y
+
+git clone https://github.com/ASHWIN990/Termux-sudo.git
+
+cd Termux-sudo
+
+cat sudo > /data/data/com.termux/files/usr/bin/sudo
+
+chmod 700 /data/data/com.termux/files/usr/bin/sudo
+
+sleep 3s
+
+clear
+
+echo -e "\e[1;91mTO USE JUST DO \e[1;91m sudo su \e[0m"
