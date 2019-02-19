f28fccca2575e4f967351a40064764a6f9a0c7bb
Ashwini Sahu <46531635+ASHWIN990@users.noreply.github.com>
2019-02-19 16:14:36 +0530

    Update install-sudo.sh

--- install-sudo.sh
+++ install-sudo.sh
@@ -1,13 +1,13 @@
+echo -e "\e[1;91mINSTALLING THE SUDO IN YOUR TERMUX\e[0m"
+
 pkg install ncurses-utils -y
 
 cat sudo > /data/data/com.termux/files/usr/bin/sudo
 
 chmod 700 /data/data/com.termux/files/usr/bin/sudo
 
+sleep 3s
+
 clear
-
-
-
-
 
 echo -e "\e[1;91mTO USE JUST DO \e[1;91m sudo su \e[0m"
