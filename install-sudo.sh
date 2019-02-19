e836ec5b1e34fd54fe57453b83a5ab0b4775b6c3
Ashwini Sahu <46531635+ASHWIN990@users.noreply.github.com>
2019-02-19 14:44:30 +0530

    Update install-sudo.sh

--- install-sudo.sh
+++ install-sudo.sh
@@ -4,10 +4,10 @@ cat sudo > /data/data/com.termux/files/usr/bin/sudo
 
 chmod 700 /data/data/com.termux/files/usr/bin/sudo
 
-cd ..
-
-rm -rf Termux-sudo
-
 clear
+
+
+
+
 
 echo -e "\e[1;91mTO USE JUST DO \e[1;91m sudo su \e[0m"
